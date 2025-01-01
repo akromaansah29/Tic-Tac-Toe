1c471ffc60d98608eee2aaf2facc7257a3f704da
akromaansah29 <ansaha@myumanitoba.ca>
2024-12-31 21:24:19 -0600

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,289 +1,169 @@
-import sys
-# GitHub Actions or automated testing environments can’t handle dynamic user inputs. We’ll need to replace input() with a mockable or test-safe alternative
 import numpy as np
 import random
-# each number represents a certain column
-col_0=[1,4,7]
-col_1=[2,5,8]
-col_2=[3,6,9]
 
 
-# Wrapper for safe input
-def safe_input(prompt):
-    """Wrapper around input() to safely handle testing environments."""
-    if 'unittest' in sys.modules:
-        return '1'  # Default value during tests
-    else:
-        return input(prompt)
+class TicTacToe:
+    def __init__(self):
+        """Initialize the game with an empty board and game state."""
+        self.board = np.array([[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']])
+        self.playerChoice = [None,None]
+        self.game_over = False
+        self.current_player = 'X'  # Default player is 'X'
+        self.coin_flip = ['H', 'T']
+        self.first_play = random.choice(self.coin_flip)
 
-def is_full(array):
-    #Parameters
-    #----------
-    #array : array you want to check if it's full
-    #Returns
-    #-------
-    #boolean
-    if (len(array[array == ' ']) == 0):
-        return True
-    else:
-        return False
-    
-    
-    
+    def is_full(self):
+        """Check if the board is full."""
+        return len(self.board[self.board == ' ']) == 0
 
-def find_row(number):
-    #Parameters
-    #----------
-    #number : user's input(between 1 and 9) to be used to find a row in the matrix(array)
-    #Returns
-    #-------
-    #int
-    
-    if number >= 1 and number <= 3:
-        return 0
-    elif number >=4 and number <= 6:
-        return 1
-    elif number >=7 and number <= 9:
-        return 2
-   
+    def find_row(self, number):
+        """Find the row corresponding to the player's choice."""
+        if 1 <= number <= 3:
+            return 0
+        elif 4 <= number <= 6:
+            return 1
+        elif 7 <= number <= 9:
+            return 2
 
-def find_column(number):
-    #Parameters
-    #----------
-    #number : user's input(between 1 and 9) to be used to find a row in the matrix(array)
-    #Returns
-    #-------
-    #int
-    
-    if number in col_0:
-        return 0
-    elif number in col_1:
-        return 1
-    elif number in col_2:
-        return 2
-    
+    def find_column(self, number):
+        """Find the column corresponding to the player's choice."""
+        col_map = {1: 0, 4: 0, 7: 0,
+                   2: 1, 5: 1, 8: 1,
+                   3: 2, 6: 2, 9: 2}
+        return col_map.get(number, -1)
 
+    def is_available(self, row, col):
+        """Check if a slot is available."""
+        return self.board[row, col] == ' '
 
-# make sure you check if row and column are valid
-def is_avaliable(array, row, column):
-    # Check for out-of-bounds indices, shape[0] = rows of array in numpy and shapes[1]=columns of array on numpy
-    if row < 0 or row >= array.shape[0] or column < 0 or column >= array.shape[1]:
-        return False
-    
-    # Check slot availability
-    return array[row, column] == ' '
-
-    
-def make_mark(array,row,column,mark):
-    #Parameters
-    #----------
-    #array : array you want to make a mark in
-    #row : the row of choice
-    #column : the column of choice
-    #mark : mark is either X or O where X represents player  and O represents computer
-    if row < 0 or row >= array.shape[0] or column < 0 or column >= array.shape[1]:
-        return  # Do nothing if out of bounds
-    array[row][column] = mark
-    
-
-def display_initial_board():
-    #no parameters
-    
-    print('   |   |    ')
-    print('---+---+----')
-    print('   |   |    ')
-    print('---+---+----')
-    print('   |   |    ')
-    
-
-# displays board with updated array values in their rightful positions
-def display_current_board(array):
-    #Parameters
-    #----------
-    #array : array you want to display
-    
-    print(' {} | {} | {}'.format(array[0][0],array[0][1],array[0][2]))
-    print('---+---+----')
-    print(' {} | {} | {}'.format(array[1][0],array[1][1],array[1][2]))
-    print('---+---+----')
-    print(' {} | {} | {}'.format(array[2][0],array[2][1],array[2][2]))
-    
-
-    
-def diagonal_win(array,mark):
-    #Parameters
-    #----------
-    #array : array you want to check for a diagonal win
-    #mark : mark is either X or O where X represents player  and O represents computer
-    #Returns
-    #----------
-    #Boolean
-    
-    if (array[0][0] == array[1][1]==array[2,2]) or (array[2,0] == array[1,1] == array[0,2]):
-        if array[1,1] == mark: #use common factor to compare
+    def make_mark(self, row, col, mark):
+        """Place a mark on the board."""
+        if self.is_available(row, col):
+            self.board[row, col] = mark
             return True
-    
-    return False
+        return False
 
-    
-def horizontal_win(array,mark):
-    #Parameters
-    #----------
-    #array : array you want to check for a horizontal win
-    #mark : mark is either X or O where X represents player  and O represents computer
-    #Returns
-    #----------
-    #Boolean
-    
-    for i in range(3): # i representing row
-        j = 0
-        if (array[i,j] == array[i,j+1] == array[i,j+2]== mark):   
-            return True
-    return False
-            
-def vertical_win(array,mark):
-    #Parameters
-    #----------
-    #array : array you want to check for a vertical win
-    #mark : mark is either X or O where X represents player  and O represents computer
-    #Returns
-    #----------
-    #Boolean
-    
-     for j in range(3): # j representing column
-        i = 0
-        if (array[i,j] == array[i+1,j] == array[i+2,j]):
-            if array[i,j] == mark:   
+    def display_current_board(self):
+        """Display the current game board."""
+        print(' {} | {} | {}'.format(self.board[0][0], self.board[0][1], self.board[0][2]))
+        print('---+---+---')
+        print(' {} | {} | {}'.format(self.board[1][0], self.board[1][1], self.board[1][2]))
+        print('---+---+---')
+        print(' {} | {} | {}'.format(self.board[2][0], self.board[2][1], self.board[2][2]))
+
+    def diagonal_win(self, mark):
+        """Check for a diagonal win."""
+        return (self.board[0, 0] == self.board[1, 1] == self.board[2, 2] == mark or
+                self.board[2, 0] == self.board[1, 1] == self.board[0, 2] == mark)
+
+    def horizontal_win(self, mark):
+        """Check for a horizontal win."""
+        for i in range(3):
+            if self.board[i, 0] == self.board[i, 1] == self.board[i, 2] == mark:
                 return True
-     return False
-            
-            
-def possible_termination(array,mark):
-    #Parameters
-    #----------
-    #array : array you want to check for a possible win or loss which would lead to termination
-    #mark : mark is either X or O where X represents player  and O represents computer
-    #Returns
-    #----------
-    #Boolean
-    
-       if diagonal_win(array,mark) == True: 
+        return False
+
+    def vertical_win(self, mark):
+        """Check for a vertical win."""
+        for j in range(3):
+            if self.board[0, j] == self.board[1, j] == self.board[2, j] == mark:
+                return True
+        return False
+
+    def possible_termination(self, mark):
+        if self.diagonal_win(mark) == True or self.horizontal_win(mark) == True or self.vertical_win(mark) : 
             if mark == 'X':
-                display_current_board(array)
+                self.display_current_board()
                 print('\n')
                 print('You win! Game Over!')
             elif mark == 'O':
-                display_current_board(array)
+                self.display_current_board()
                 print('\n')
                 print('You lose!')
             print('Play again? Press Enter to play again. ') 
             print('\n')
             print('**END OF PROGRAMMING**') 
-                 
-            return True    
-                
-       elif horizontal_win(array,mark) == True:
-            if mark == 'X':
-                display_current_board(array)
-                print('\n')
-                print('You win! Game Over!')
-            elif mark == 'O':
-                display_current_board(array)    # i wanted the player to see the board after the loss.(to see exactly what happened)
-                print('\n')
-                print('You lose!')
-            print('Play again? Press Enter to play again. ') 
-            print('\n')
-            print('**END OF PROGRAMMING**')     
-            return True  
-            
-       elif vertical_win(array,mark) == True:
-            if mark == 'X':
-                display_current_board(array)
-                print('\n')
-                print('You win! Game Over!')
-            elif mark == 'O':
-                display_current_board(array)
-                print('\n')
-                print('You lose!')
-            print('Play again? Press Enter to play again. ') 
-            print('\n')
-            print('**END OF PROGRAMMING**')     
-            return True  
-        
-       else:
-            return False
+            self.game_over = True
        
        
-    
-    
-    
-    
-print('Welcome to the tick tak toe Game') 
-print('===================================')
-game_over = False
-arry = np.array([[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']])
-coin_flip = ['H','T'] #coin flip to select who plays first
-first_play = random.choice(coin_flip)
 
-if first_play == 'H':
-    print('You go first. Your letter is X.') 
 
-    
-else:
-    
-    
-    computer_choice = random.randint(1,9)
-    row = find_row(computer_choice)
-    column = find_column(computer_choice)
-    make_mark(arry,row,column,'O')
+    def play_turn(self, choice, mark):
+        """Play a turn for the given mark."""
+        row = self.find_row(choice)
+        col = self.find_column(choice)
+        if self.make_mark(row, col, mark):
+            return self.possible_termination(mark)
+        print(f"Slot {choice} is not available. Try again.")
+        return False
 
-    
-while game_over == False:
-    
-        if is_full(arry) == False:
-            display_current_board(arry)   
-            player_choice = safe_input('Pick an open slot: ')
-            
-            while not(int(player_choice) >= 1 and int(player_choice) <= 9):
-                print("That's not an open slot.")
-                display_current_board(arry)
-                player_choice = safe_input('Pick an open slot: ')
-                
+    def validate_playerChoice(self):
         
-            row = find_row(int(player_choice))    
-            col = find_column(int(player_choice))
-        
-            while is_avaliable(arry,row,col) == False:
-                print("That's not an open slot.")
-                display_current_board(arry)
-                player_choice = safe_input('Pick an open slot: ')
-                row = find_row(int(player_choice))    
-                col = find_column(int(player_choice))
-                
-            make_mark(arry,row,col,'X') 
-            game_over = possible_termination(arry,'X')
+        player_choice = input('Pick an open slot:') 
+        while not(int(player_choice) >= 1 and int(player_choice) <= 9):
+            print("That's not an open slot.")
+            self.display_current_board()
+            player_choice = input('Pick an open slot: ')
             
-            if game_over == False:
-                
-                computer_choice = random.randint(1,9)
-                row = find_row(computer_choice)
-                col = find_column(computer_choice)
-                
-                while is_avaliable(arry,row,col) == False:
-                    computer_choice = random.randint(1,9)
-                    row = find_row(computer_choice)
-                    col = find_column(computer_choice)
-                    
-               
-                make_mark(arry,row,col,'O')
-                game_over = possible_termination(arry,'O')
-                
+        row = self.find_row(int(player_choice))    
+        col = self.find_column(int(player_choice))
+
+        while self.is_available(row,col) == False:
+            print("That's not an open slot.")
+            self.display_current_board()
+            player_choice = input('Pick an open slot: ')
+            row = self.find_row(int(player_choice))    
+            col = self.find_column(int(player_choice))
+           
+        self.playerChoice = [row,col]
+        
+        
+
+    def play_game(self):
+        """Main interactive game logic."""
+        print('Welcome to Tic Tac Toe!')
+        print('===================================')
+        if self.first_play == 'H':
+            print('You go first. Your letter is X.') 
         else:
-             print('Tie')
-             print('Play again? Press Enter to play again.')   
-             print('\n')
-             print('**END OF PROGRAMMING**')   
-             game_over = True
+            computer_choice = random.randint(1,9)
+            row = self.find_row(computer_choice)
+            column = self.find_column(computer_choice)
+            self.make_mark(row,column,'O')
+            
+        while self.game_over  == False:
+           
+           if self.is_full() == False:
+                self.display_current_board()   
+                self.validate_playerChoice()
+                choice  = self.playerChoice
+                self.make_mark(choice[0],choice[1],'X') 
+                self.possible_termination('X')
+                if self.game_over == False:
+                    computer_choice = random.randint(1,9)
+                    row = self.find_row(computer_choice)
+                    col = self.find_column(computer_choice)
+                    while self.is_available(row,col) == False:
+                        computer_choice = random.randint(1,9)
+                        row = self.find_row(computer_choice)
+                        col = self.find_column(computer_choice)
+                    self.make_mark(row,col,'O')
+                    self.possible_termination('O')
+           else:
+                print('Tie')
+                print('Play again? Press Enter to play again.')   
+                print('\n')
+                print('**END OF PROGRAMMING**')   
+                self.game_over = True
+           
+           
 
-     
-    
+ 
+            
+
+
+
+# Run the game
+if __name__ == "__main__":
+    game = TicTacToe()
+    game.play_game()
